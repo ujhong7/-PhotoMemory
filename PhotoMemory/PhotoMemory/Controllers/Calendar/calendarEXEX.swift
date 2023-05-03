1489402b814b4b29d4d659834896cef5ffe5313b
ujhong7 <ujhong7@gmail.com>
2023-05-03 20:10:16 +0900

    no message

---
+++ PhotoMemory/PhotoMemory/Controllers/Calendar/calendarEXEX.swift
@@ -0,0 +1,17 @@
+//
+//  calendarEXEX.swift
+//  PhotoMemory
+//
+//  Created by yujaehong on 2023/05/03.
+//
+
+import UIKit
+
+class calendarEXEX: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGray6
+    }
+    
+}
--- PhotoMemory/PhotoMemory/Views/FeedCell.swift
+++ PhotoMemory/PhotoMemory/Views/FeedCell.swift
@@ -15,9 +15,7 @@ class FeedCell: UICollectionViewCell {
         }
     }
     
-    
     // MARK: - LifeCycle
-    
     override init(frame: CGRect) {
         super.init(frame: frame)
         // backgroundColor = .red
