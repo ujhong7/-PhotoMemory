aeb087b01bf01d676e9a081092a787a67a8cefbf
Kant <eileenyoo2@gmail.com>
2023-06-09 23:08:34 +0900

    - OnboardingViewController 코드 수정 및 정리

--- PhotoMemory/PhotoMemory/Controllers/OnboardingViewController.swift
+++ PhotoMemory/PhotoMemory/Controllers/OnboardingViewController.swift
@@ -24,11 +24,12 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
     }
 
     func setupScrollView() {
-        scrollView = UIScrollView(frame: view.bounds)
+        scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
         scrollView.delegate = self
         scrollView.isPagingEnabled = true
         scrollView.showsHorizontalScrollIndicator = false
-        scrollView.contentSize = CGSize(width: view.bounds.width * CGFloat(images.count), height: view.bounds.height)
+        scrollView.contentSize = CGSize(width: view.bounds.width * CGFloat(images.count), height: scrollView.bounds.height)
 
         for i in 0..<images.count {
             let frame = CGRect(x: view.bounds.width * CGFloat(i), y: 0, width: view.bounds.width, height: view.bounds.height)
@@ -37,11 +38,16 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
         }
 
         view.addSubview(scrollView)
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
     }
 
     func createContentView(frame: CGRect, imageName: String) -> UIView {
         let contentView = UIView(frame: frame)
-
         let imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: frame.width, height: frame.height))
         imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
@@ -52,7 +58,8 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
     }
 
     func setupPageControl() {
-        pageControl = UIPageControl(frame: CGRect(x: 0, y: view.bounds.height - 50, width: view.bounds.width, height: 50))
+        pageControl = UIPageControl()
+        pageControl.translatesAutoresizingMaskIntoConstraints = false
         pageControl.numberOfPages = images.count
         pageControl.currentPage = 0
         pageControl.tintColor = .black
@@ -61,6 +68,13 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
         pageControl.addTarget(self, action: #selector(pageControlValueChanged(_:)), for: .valueChanged)
 
         view.addSubview(pageControl)
+        NSLayoutConstraint.activate([
+            pageControl.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            pageControl.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -100),
+            pageControl.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            pageControl.widthAnchor.constraint(equalToConstant: view.bounds.width),
+            pageControl.heightAnchor.constraint(equalToConstant: 50)
+        ])
     }
 
     @objc func pageControlValueChanged(_ sender: UIPageControl) {
@@ -72,13 +86,9 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         let pageIndex = round(scrollView.contentOffset.x / scrollView.bounds.width)
         pageControl.currentPage = Int(pageIndex)
-
-        if pageIndex == CGFloat(images.count - 1) {
-            showStartButton()
-        } else {
-            hideStartButton()
-        }
-
+        
+        pageIndex == CGFloat(images.count - 1) ? showStartButton() : hideStartButton()
+        
         if pageIndex == 0 {
             scrollView.isScrollEnabled = scrollView.contentOffset.x >= 0
         } else {
@@ -98,6 +108,7 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
 
             let contentView = createContentView(frame: lastPageFrame, imageName: images[lastPageIndex])
             startButton = UIButton(type: .system)
+            startButton.translatesAutoresizingMaskIntoConstraints = false
             startButton.setTitle("시작하기", for: .normal)
             startButton.titleLabel?.font = UIFont.boldSystemFont(ofSize: 18)
             startButton.frame = CGRect(x: 20, y: view.bounds.height - 150, width: view.bounds.width - 40, height: 50)
@@ -106,9 +117,15 @@ class OnboardingViewController: UIViewController, UIScrollViewDelegate {
             startButton.tintColor = .white
 
             startButton.addTarget(self, action: #selector(startButtonTapped), for: .touchUpInside)
-            contentView.addSubview(startButton)
-
-            scrollView.addSubview(contentView)
+            
+            view.addSubview(startButton)
+            NSLayoutConstraint.activate([
+                startButton.topAnchor.constraint(equalTo: scrollView.bottomAnchor, constant: 0),
+                startButton.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+                startButton.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+                startButton.widthAnchor.constraint(equalToConstant: view.bounds.width),
+                startButton.heightAnchor.constraint(equalToConstant: 50)
+            ])
         }
     }
 
